706a5f42ccbc3b2faa4dc43268adf85d92bf7f79
maciejfed <mfedorowiat@gmail.com>
2020-02-12 23:04:18 +0100

    Test

--- src/com/fedorowiat/App.java
+++ src/com/fedorowiat/App.java
@@ -6,6 +6,6 @@ import java.util.Date;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Dzisiaj jest: "+new Date());
+        System.out.println("Dzisiaj jest sobota");
     }
 }
