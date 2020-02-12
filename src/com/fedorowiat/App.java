b4f65279df775e1807bb12b9a15c971d7c30ca9a
daniel.fedorowiat <daniel.fedorowiat@gmail.com>
2020-02-12 23:01:21 +0100

    Time

--- src/com/fedorowiat/App.java
+++ src/com/fedorowiat/App.java
@@ -1,9 +1,11 @@
 package com.fedorowiat;
 
 import java.util.Arrays;
+import java.util.Date;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Dzisiaj jest: "+new Date());
     }
 }
