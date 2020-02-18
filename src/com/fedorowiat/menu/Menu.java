e40c17d6ece0e73366cdeb648f50eee02028f42a
daniel.fedorowiat <daniel.fedorowiat@gmail.com>
2020-02-18 16:24:22 +0100

    class update

--- src/com/fedorowiat/menu/Menu.java
+++ src/com/fedorowiat/menu/Menu.java
@@ -1,17 +1,20 @@
 package com.fedorowiat.menu;
 
+import com.fedorowiat.task.Draw;
+
 import java.util.Scanner;
 
 public class Menu {
     Scanner scanner = new Scanner(System.in);
-
+    Draw draw = new Draw();
     public void showMenu(){
         System.out.println("Select a task: \n" +
                 "1.Draw a Christmas tree.");
         int choice = scanner.nextInt();
         switch (choice){
             case(1):
-
+            draw.drawAChritmasTree();
+            break;
             default:
                 throw new IllegalStateException("Unexpected value: " + choice);
         }
--- src/com/fedorowiat/task/Draw.java
+++ src/com/fedorowiat/task/Draw.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Draw {
 
-    public void drawAChritmasTree(){
+    public void drawAChritmasTree() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Podaj wielkośc choinki: ");
         int hight = scanner.nextInt();
@@ -14,18 +14,35 @@ public class Draw {
         for (int i = 0; i < tab.length; i++) {
             tab[i] = '*';
         }
+        int space = hight;
+        int increase = 1;
         for (int i = 1; i <= hight; i++) {
+
             if (i == 1) {
+                for (space = hight; space > 0; space--) {
+                    System.out.print(" ");
+                }
                 System.out.print(tab[i]);
             } else {
                 System.out.println("");
+                for (space = (hight - increase); space > 0; space--) {
+                    System.out.print(" ");
+                }
                 for (int k = 0; k < (2 * i - 1); k++) {
                     System.out.print(tab[i]);
                 }
+                increase++;
+
             }
+
         }
 
     }
 
 
 }
+
+
+
+
+
